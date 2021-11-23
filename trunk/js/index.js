858712489b913fe978323b1c394c3ce65daa05a1
liuyijingjing <2191332122@qq.com>
2021-11-23 19:28:37 +0800

    这是第七周的项目

--- trunk/js/index.js
+++ trunk/js/index.js
@@ -81,16 +81,67 @@ $(function() {
         controlsColor:"#d7d7d7",//普通控制按钮的颜色
         controlsCurrentColor:"green",//当前控制按钮的颜色
     });
-/* 推广产品切换*/
-$('.promotion .title ul li').mouseenter(function() {
-    //导航激活类的切换
-    $(this).addClass('active').siblings().removeClass('active')
+    /* 推广产品切换*/
+    $('.promotion .title ul li').mouseenter(function() {
+        //导航激活类的切换
+         $(this).addClass('active').siblings().removeClass('active')
         //内容切换
         //获取对应index
-    var index = $(this).index();
-    //左右移动
-    $('.promotion .promotion-content  .inner-box').animate({
+        var index = $(this).index();
+        //左右移动
+        $('.promotion .promotion-content  .inner-box').animate({
         'left': -index * 1170
     })
-})
-})
+    })
+
+
+
+   /*二维码滑出效果 */
+  $('.qr-code .ticket').hover(function(){
+    //让二维码滑出来
+    $('.qr-code div').stop(true).animate({
+      left:'-100px'
+    })
+   },function(){
+      //让二维码收进去
+        $('.qr-code div').stop(true).animate({
+        left: 0
+        })
+   })
+
+
+
+   /*顶部搜索框交互 */
+   $(document).scroll(function (){
+     //获取到顶部的距离
+     var topDistance = $('html,bady').scrollTop();
+     //判断
+      if (topDistance > 500) {
+      //如果滚动距离大于500  滑下来
+        $('.top-search-box').slideDown()
+     }else {
+      //否则  收回去
+        $('.top-search-box').slideUp() 
+      }
+   })
+    
+   
+
+   /*楼梯效果*/ 
+   $('.floor li').click(function () {
+      //获取索引
+      var index = $(this).index();
+      //选中每一个板块到顶部偏移
+      var topOffset = $('.floorBox').eq(index).offset().top;
+
+      //让滚动条滚动到这个位置
+      $('html,body').animate({
+          scrollTop: topOffset -50
+      })
+    })
+
+
+
+
+   
+}) 
